f1d18f07c73eda5f634770fdd7cefc7ad899933c
GaaneshT <dstworstsubject@gmail.com>
2025-02-20 00:54:58 +0800

    Add assert cases

--- src/main/java/malt/task/Task.java
+++ src/main/java/malt/task/Task.java
@@ -12,6 +12,7 @@ public abstract class Task {
      * @param description The description of the task.
      */
     public Task(String description) {
+        assert description != null && !description.trim().isEmpty() : "Task description cannot be null or empty!";
         this.description = description;
         this.isDone = false;
     }
